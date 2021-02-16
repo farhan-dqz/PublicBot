629c6cc7a97c3c9c1cfce3120df32aefda2b79cd
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-02-16 18:46:57 +0530

    Update deepai.js

--- plugins/deepai.js
+++ plugins/deepai.js
@@ -19,13 +19,13 @@ deepai.setApiKey('4ec4c7f4-63cd-457f-b244-7e12bba7ebde'); // Quickstart API Key
 const Language = require('../language'); 
 const Lang = Language.getString('deepai'); // Language Support
 
-Asena.addCommand({pattern: 'deepai', fromMe: true, deleteCommand: false, desc: Lang.DEEPAI_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'deepai', fromMe: false, deleteCommand: false, desc: Lang.DEEPAI_DESC}, (async (message, match) => {
 
     await message.sendMessage('💻 Usage: *.moodai <text>*\nℹ️ Desc: 🇹🇷 Yazdığınız yazıdan ruh halinizi bulur.\n🇬🇧 It finds your mood from the article you wrote.\n\n💻 Usage: *.colorai*\nℹ️ Desc: 🇹🇷 Siyah beyaz fotoğrafları renklendirir.\n🇬🇧 It colorize bw photos.\n\n💻 Usage: *.superai*\nℹ️ Desc: 🇹🇷 Fotoğrafın kalitesini yapay zeka ile arttırır.\n🇬🇧 Improves the quality of photos with Neural AI.\n\n💻 Usage: *.waifuai*\nℹ️ Desc: 🇹🇷 Fotoğrafların renk paletlerini yapay zeka ile birleştirir.\n🇬🇧 Combines the color palettes of photos with artificial intelligence.\n\n💻 Usage: *.dreamai*\nℹ️ Desc: 🇹🇷 Fotoğrafa deepdream efekti uygular.\n🇬🇧 Applies deepdream effect to the photo.\n\n💻 Usage: *.neuraltalkai*\nℹ️ Desc: 🇹🇷 Fotoğrafki olan şeyi yapay zeka ile açıklar.\n🇬🇧 Explain the phenomenon in the photo with artificial intelligence.\n\n💻 Usage: *.ttiai <text>*\nℹ️ Desc: 🇹🇷 Yazıyı resme dönüştürür.\n🇬🇧 Converts text to a picture. (Text-to-Image)\n\n💻 Usage: *.toonai*\nℹ️ Desc: 🇹🇷 Fotoğraftaki yüzü çizgi film karakterine çevirir.\n🇬🇧 Turns the face in the photo into a cartoon character.\n\n💻 Usage: *.textai <text>*\nℹ️ Desc: 🇹🇷 Yazdığınız cümleden size yapay bir hikaye yaratır.\n🇬🇧 It creates an artificial story for you from your sentence.\n\n💻 Usage: *.nudityai*\nℹ️ Desc: 🇹🇷 Fotoğraftaki NSFW değerini 1 ve 0 arasında gösterir. \n🇬🇧 It shows the NSFW value between 1 and 0 in the photo.\n\n💻 Usage: *.ganstyle*\nℹ️ Desc: 🇹🇷 Yanıtladığınız fotoğrafı seçili olan resim ile birleştirir.\n🇬🇧 Combines the photo you answered with the selected picture.\n\n⚠️ 🇹🇷 *Bütün bu yapay zeka araçlarını derin öğrenme ile çalışır. Ne kadar fazla kullanırsanız o kadar fazla bilgiyi depolar.* ```Sadece ingilizce karakter kullanın!```\n\n⚠️ 🇬🇧 *All the tools here work with deep learning. The more you use it, the more information it stores.* ```Use only english characters!```');
 
 }));
 
-Asena.addCommand({pattern: 'colorai', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
+Asena.addCommand({pattern: 'colorai', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage('```Need Photo!```');
 
     var downloading = await message.client.sendMessage(message.jid,'Colorizing.. 🎨',MessageType.text);
@@ -55,7 +55,7 @@ Asena.addCommand({pattern: 'colorai', fromMe: true, deleteCommand: false, dontAd
 
 }));
 
-Asena.addCommand({pattern: 'waifuai', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
+Asena.addCommand({pattern: 'waifuai', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage('```Need Photo!```');
 
     var downloading = await message.client.sendMessage(message.jid,'Mixing.. 🧩',MessageType.text);
@@ -85,7 +85,7 @@ Asena.addCommand({pattern: 'waifuai', fromMe: true, deleteCommand: false, dontAd
 
 }));
 
-Asena.addCommand({pattern: 'superai', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
+Asena.addCommand({pattern: 'superai', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage('```Need Photo!```');
 
     var downloading = await message.client.sendMessage(message.jid,'Enhancing.. 🖌️',MessageType.text);
@@ -115,7 +115,7 @@ Asena.addCommand({pattern: 'superai', fromMe: true, deleteCommand: false, dontAd
 
 }));
 
-Asena.addCommand({pattern: 'moodai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'moodai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.TEXT);
 
     var resp = await deepai.callStandardApi("sentiment-analysis", {
@@ -127,7 +127,7 @@ Asena.addCommand({pattern: 'moodai ?(.*)', fromMe: true, deleteCommand: false, d
 
 }));
 
-Asena.addCommand({pattern: 'dreamai', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
+Asena.addCommand({pattern: 'dreamai', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage('```Need Photo!```');
 
     var downloading = await message.client.sendMessage(message.jid,'Starry Night.. 🌃',MessageType.text);
@@ -157,7 +157,7 @@ Asena.addCommand({pattern: 'dreamai', fromMe: true, deleteCommand: false, dontAd
 
 }));
 
-Asena.addCommand({pattern: 'neuraltalkai', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
+Asena.addCommand({pattern: 'neuraltalkai', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage('```Need Photo!```');
 
     var downloading = await message.client.sendMessage(message.jid,'Reading.. 🙇🏻',MessageType.text);
@@ -185,7 +185,7 @@ Asena.addCommand({pattern: 'neuraltalkai', fromMe: true, deleteCommand: false, d
 
 }));
 
-Asena.addCommand({pattern: 'ttiai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'ttiai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.TEXT);
 
     var resp = await deepai.callStandardApi("text2img", {
@@ -199,7 +199,7 @@ Asena.addCommand({pattern: 'ttiai ?(.*)', fromMe: true, deleteCommand: false, do
 
 }));
 
-Asena.addCommand({pattern: 'toonai', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
+Asena.addCommand({pattern: 'toonai', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage('```Need Photo!```');
 
     var downloading = await message.client.sendMessage(message.jid,'Tooning.. 🌟',MessageType.text);
@@ -229,7 +229,7 @@ Asena.addCommand({pattern: 'toonai', fromMe: true, deleteCommand: false, dontAdd
 
 }));
 
-Asena.addCommand({pattern: 'nudityai', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
+Asena.addCommand({pattern: 'nudityai', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage('```Need Photo!```');
 
     var downloading = await message.client.sendMessage(message.jid,'Finding NSFW.. 🔥',MessageType.text);
@@ -257,7 +257,7 @@ Asena.addCommand({pattern: 'nudityai', fromMe: true, deleteCommand: false, dontA
 
 }));
 
-Asena.addCommand({pattern: 'textai ?(.*)', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'textai ?(.*)', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {
     if (match[1] === '') return await message.sendMessage(Lang.TEXT);
 
     var resp = await deepai.callStandardApi("text-generator", {
@@ -269,7 +269,7 @@ Asena.addCommand({pattern: 'textai ?(.*)', fromMe: true, deleteCommand: false, d
 
 }));
 
-Asena.addCommand({pattern: 'ganstyle', fromMe: true, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
+Asena.addCommand({pattern: 'ganstyle', fromMe: false, deleteCommand: false, dontAddCommandList: true}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage('```Need Photo!```');
 
     var downloading = await message.client.sendMessage(message.jid,'Creating.. ♻️',MessageType.text);
