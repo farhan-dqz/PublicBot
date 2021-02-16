df9af51d732c40c905c17705abb4bb904aabeb52
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-02-16 19:00:58 +0530

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -11,7 +11,7 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('ttp');
 
-Asena.addCommand({pattern: 'ttp ?(.*)', fromMe: true, desc: Lang.TTP_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'ttp ?(.*)', fromMe: false, desc: Lang.TTP_DESC}, (async (message, match) => {
 
     if (match[1] === '') return await message.sendMessage(Lang.NEED_WORD);
 
