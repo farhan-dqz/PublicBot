54812badceaf927645a95d8b58b4f33f95827d11
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-02-23 00:10:45 +0530

    Update screenshot.js

--- plugins/screenshot.js
+++ plugins/screenshot.js
@@ -7,16 +7,18 @@ const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
+const request = require('request');
+const got = require("got");
 
 const Language = require('../language');
 const Lang = Language.getString('webss');
 
-Asena.addCommand({pattern: 'ss ?(.*)', fromMe: false, desc: Lang.SS_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'ss ?(.*)', fromMe: true, desc: Lang.SS_DESC}, (async (message, match) => {
 
     if (match[1] === '') return await message.sendMessage(Lang.LİNK);
 
-    var webimage = await axios.get(`https://nurutomo.herokuapp.com/api/ssweb?delay=1000&url=${match[1]}`, { responseType: 'arraybuffer' })
+    var webimage = await axios.get(`https://screenshotapi.net/api/v1/screenshot?url=${match[1]}&output=image&full_page=true`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAsena'})
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg, caption: 'Made by WhatsAsena'})
 
 }));
