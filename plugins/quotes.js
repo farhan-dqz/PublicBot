9980b2e1eeb58bab4c3749de511d851880ae11b5
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-02-23 22:22:37 +0530

    Update quotes.js

--- plugins/quotes.js
+++ plugins/quotes.js
@@ -11,7 +11,7 @@ const got = require('got');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
-Asena.addCommand({pattern: 'quotes ?(.*)', fromMe: false, desc: Lang.QUOTE_DESC}, async (message, match) => {
+Asena.addCommand({pattern: 'quote ?(.*)', fromMe: false, desc: Lang.QUOTE_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.quotable.io/random`;
 	try {
