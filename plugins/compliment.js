4d65757873854ba11cfeaa0df9179daf90aff04a
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-02-24 23:59:11 +0530

    Update compliment.js

--- plugins/compliment.js
+++ plugins/compliment.js
@@ -12,12 +12,12 @@ const Language = require('../language');
 const Lang = Language.getString('weather');
 
 Asena.addCommand({pattern: 'complimentme ?(.*)', fromMe: false, desc: Lang.CM_DESC}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONAC);
+	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://complimentr.com/api`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📌 ' + Lang.CM +'* ```' + json.compliment + '```\n\n' , MessageType.text);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🤗 ' + Lang.CM +'* ```' + json.compliment + '```\n\n' , MessageType.text);
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 	}
