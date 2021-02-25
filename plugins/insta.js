9cddd58e60b760c9b9dd8f4802607a0165069f9a
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-02-25 13:23:58 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -19,7 +19,7 @@ Asena.addCommand({pattern: 'insta ?(.*)', fromMe: false, usage: Lang.USAGE, desc
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-		/*if (response.statusCode === 200)*/ return await message.client.sendMessage(message.jid, '*🏷' + Lang.USERNAME +':* ```' + match[1] + '```\n\n' +
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🏷' + Lang.USERNAME +':* ```' + match[1] + '```\n\n' +
 		'*🔖 ' + Lang.NAME +':* ```' + json.graphql.user.full_name + '```\n' + 
 		'*📄 ' + Lang.BIO +':* ```' + json.graphql.user.biography + '```\n' +
 		'*👣 ' + Lang.FOLLOWERS +':* ```' + json.graphql.user.edge_followed_by.count + '```\n' + 
