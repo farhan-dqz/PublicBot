084d729a514d535a24f146d45a0a5ef15dc801ce
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-02-25 13:23:02 +0530

    Update qrgenerator.js

--- plugins/qrgenerator.js
+++ plugins/qrgenerator.js
@@ -13,7 +13,7 @@ const got = require("got");
 const Language = require('../language');
 const Lang = Language.getString('webss');
 
-Asena.addCommand({pattern: 'qr ?(.*)', fromMe: true, desc: Lang.QR_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'qr ?(.*)', fromMe: false, desc: Lang.QR_DESC}, (async (message, match) => {
 
     if (match[1] === '') return await message.sendMessage(Lang.TEXT);
 
