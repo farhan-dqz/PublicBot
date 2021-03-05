2b4c0e95e28764ed835f37cefcc33ecc1401c1b6
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-05 23:32:09 +0530

    Update joox.js

--- plugins/joox.js
+++ plugins/joox.js
@@ -22,7 +22,8 @@ Asena.addCommand({pattern: 'joox ?(.*)', fromMe: false, desc: Lang.JOOX_DESC}, a
 		'*🔊 ' + Lang.TITLE +':* ```' + json.result.judul + '```\n' +
 		'*🎚️ ' + Lang.PUBLICATION +':* ```' + json.result.dipublikasi + '```\n' + 
 		'*🎙️ ' + Lang.SONGL +':* ```' + json.result.mp3 + '```\n' , MessageType.text);
-		 await message.sendMessage(Buffer.from(json.result.mp3), MessageType.audio, {mimetype: Mimetype.mp4Audio });
+		 await message.sendMessage(Buffer.from(json.result.mp3), MessageType.document, {mimetype: Mimetype.mp3 ,ptt: true });
+		
 		
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDS, MessageType.text);
