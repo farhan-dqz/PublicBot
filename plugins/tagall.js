06d16a111a5ab44a2dfc35af6f63dad44a125188
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-09 20:52:03 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -10,18 +10,18 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Language = require('../language');
 const Lang = Language.getString('tagall');
 
-async function checkImAdmin(message, user = message.client.user.jid) {
+/*async function checkImAdmin(message, user = message.client.user.jid) {
     var grup = await message.client.groupMetadata(message.jid);
     var sonuc = grup['participants'].map((member) => {
         if (member.id.split('@')[0] === user.split('@')[0] && member.isAdmin) return true; else; return false;
     });
     return sonuc.includes(true);
-}
+}*/
 
 Asena.addCommand({pattern: 'tagall', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
 
-   var im = await checkImAdmin(message);
-   if (!im) return await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+  /* var im = await checkImAdmin(message);
+   if (!im) return await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})*/
    /*message.client.sendMessage(message.jid,Lang.ADMİN,MessageType.text);*/
 
     grup = await message.client.groupMetadata(message.jid);
