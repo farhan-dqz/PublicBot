2f1aa31157414355fa009af23561b118f299bef2
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-09 23:59:07 +0530

    Update tinyurl.js

--- plugins/tinyurl.js
+++ plugins/tinyurl.js
@@ -24,7 +24,7 @@ Asena.addCommand({pattern: 'tiny ?(.*)', fromMe: false, desc: Lang.TIN_DESC}, as
 	}
 });
 
-Asena.addCommand({pattern: 'tiny ?(.*)', fromMe: true, desc: Lang.TIN_DESC}, async (message, match) => {
+Asena.addCommand({pattern: 'ptiny ?(.*)', fromMe: true, desc: Lang.TIN_DESC}, async (message, match) => {
 	if (match[1] === '') return await message.reply(Lang.NEED_LINK);
 	const url = `https://tobz-api.herokuapp.com/api/tinyurl?url=${match[1]}&apikey=BotWeA`;
 	try {
