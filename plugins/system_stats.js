d99cae3d691c6341b4b6bbaf14c15f80a94ed036
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-09 23:07:59 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -31,3 +31,20 @@ Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async 
         '```' + child + '```', MessageType.text
     );
 }));
+
+
+Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+    if (Config.ALIVEMSG == 'default') {
+        await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/whatsasenaremaster' , MessageType.text);
+    }
+    else {
+        await message.client.sendMessage(message.jid,Config.ALIVEMSG, MessageType.text);
+    }
+}));
+
+Asena.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
+    const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+    await message.sendMessage(
+        '```' + child + '```', MessageType.text
+    );
+}));
