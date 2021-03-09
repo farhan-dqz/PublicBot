5a882bd8ba2b8b373bb27963d29ef483822817b9
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-10 00:03:45 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -13,7 +13,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Language = require('../language');
 const Lang = Language.getString('_asena');
 
-Asena.addCommand({pattern: 'commands ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'help ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
     var CMD_HELP = '';
     if (match[1] === '') {
         Asena.commands.map(
@@ -78,7 +78,7 @@ Asena.addCommand({pattern: 'commands ?(.*)', fromMe: false, dontAddCommandList: 
 }));
 
 /*slf*/
-Asena.addCommand({pattern: 'commands ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+Asena.addCommand({pattern: 'phelp ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
     var CMD_HELP = '';
     if (match[1] === '') {
         Asena.commands.map(
