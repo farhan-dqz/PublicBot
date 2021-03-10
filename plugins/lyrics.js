8541fa93509357157a15c66c39b124a3e3a9cc21
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-10 19:22:34 +0530

    Update lyrics.js

--- plugins/lyrics.js
+++ plugins/lyrics.js
@@ -9,21 +9,24 @@ const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
 
 const Language = require('../language');
-const Lang = Language.getString('weather');
+const Lang = Language.getString('weather');          
 
 Asena.addCommand({pattern: 'lyrics ?(.*)', fromMe: false, desc: Lang.LYRICS_DESC}, async (message, match) => {
 	if (match[1] === '') return await message.reply(Lang.NEED_SONG);
-	const url = `https://tobz-api.herokuapp.com/api/lirik?q=${match[1]}&apikey=BotWeA`;
+	const url = `https://videfikri.com/api/liriklagu/?query=${match[1]}`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
 		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🎙️  ' + Lang.SONG +':* ```' + match[1] + '```\n\n' +
-		'*🎧 ' + Lang.ALBUM +':* ```' + json.result.album + '```\n' + 
-		'*🔊 ' + Lang.TITLE +':* ```' + json.result.judul + '```\n' +
-		'*🎚️ ' + Lang.PUBLICATION +':* ```' + json.result.dipublikasi + '```\n' + 
+		/*'*🎧 ' + Lang.ALBUM +':* ```' + json.result.album + '```\n' +*/
+		'*🔊 ' + Lang.TITLE +':* ```' + json.result.title + '```\n' +
+		'*🎚️ ' + Lang.PUBLICATION +':* ```' + json.result.artist + '```\n' + 
 		'*🎼 ' + Lang.SONGLI +':* ```' + json.result.lirik + '```\n' , MessageType.text);
 		
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDS, MessageType.text);
 	}
 });
+
+
+/*https://tobz-api.herokuapp.com/api/lirik?q=${match[1]}&apikey=BotWeA*/
