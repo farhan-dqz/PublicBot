a91005b441a9d2cf4d425796c56bfa16aaf8eb77
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-10 17:38:52 +0530

    Update instagram.js

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -38,26 +38,26 @@ Asena.addCommand({ pattern: 'insta ?(.*)', fromMe: false, usage: Lang.USAGE, des
     await message.sendMessage(infoMessage(Lang.LOADING))
 
     await axios
-      .get(`https://www.instagram.com/${userName}/?__a=1`)
+      .get(`https://videfikri.com/api/igstalk/?username=${userName}`)
       .then(async (response) => {
         const {
-          profile_pic_url_hd,
+          profile_hd,
           username,
-          biography,
-          edge_followed_by,
-          edge_follow,
+          bio,
+          followers,
+          following,
           full_name,
           is_private,
-        } = response.data.graphql.user
+        } = response.data.result
 
-        const profileBuffer = await axios.get(profile_pic_url_hd, {responseType: 'arraybuffer'})
+        const profileBuffer = await axios.get(profile_hd, {responseType: 'arraybuffer'})
 
         const msg = `
         *${Lang.NAME}*: ${full_name}
         *${Lang.USERNAME}*: ${username}
-        *${Lang.BIO}*: ${biography}
-        *${Lang.FOLLOWERS}*: ${edge_followed_by.count}
-        *${Lang.FOLLOWS}*: ${edge_follow.count}
+        *${Lang.BIO}*: ${bio}
+        *${Lang.FOLLOWERS}*: ${followers}
+        *${Lang.FOLLOWS}*: ${following}
         *${Lang.ACCOUNT}*: ${is_private ? Lang.HIDDEN : Lang.PUBLIC}`
 
         await message.sendMessage(Buffer.from(profileBuffer.data), MessageType.image, {
