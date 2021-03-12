2d285a6a7f8cc77cf778b6c479438764d4d2acd0
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-12 19:24:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -40,6 +40,6 @@ module.exports = {
     SUDO: process.env.SUDO === undefined ? false : process.env.SUDO,
     DEBUG: DEBUG,
     COFFEEHOUSE_API_KEY: process.env.COFFEEHOUSE_API_KEY === undefined ? false : process.env.COFFEEHOUSE_API_KEY,
-    WITAI_API: "TEYMELA6DMC4XB5YM3SPTTQWUUIBKURG"
+    WITAI_API: "722KEMAUNTE5CO2KU4B7KPBMZ5YGQFE7"
     
 };
