5d1836ff01a7015ef913f38103a0027b509df138
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-16 09:10:33 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -18,7 +18,7 @@ const Lang = Language.getString('tagall');
     return sonuc.includes(true);
 }*/
 
-Asena.addCommand({pattern: 'tagall', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'tagall', fromMe: true, desc: Lang.TAGALL_DESC, dontAddCommandList: true}, (async (message, match) => {
 
   /*var im = await checkImAdmin(message);
    if (!im) return await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})*/
