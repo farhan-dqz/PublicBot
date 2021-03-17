fc1622b9991df727da86af036103316524ea7436
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-17 20:39:30 +0530

    Update simsimih.js

--- plugins/simsimih.js
+++ plugins/simsimih.js
@@ -17,7 +17,7 @@ Asena.addCommand({pattern: 'bot ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, asy
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*🤖 ' + Lang.BOT +'* ```' + json.result.jawaban + '```\n\n' , MessageType.text,{quoted: message.data});
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*🤖 ' + Lang.BOT +'* ```' + json.result.jawaban + '```\n\n' , MessageType.text,{quoted: message.data});
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 	}
