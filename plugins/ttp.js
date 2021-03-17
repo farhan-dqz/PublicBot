0278196e0f3e15abc195dd9049b4093a8bb29ef3
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-17 21:10:26 +0530

    Update ttp.js

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -13,7 +13,7 @@ const Lang = Language.getString('ttp');
 
 Asena.addCommand({pattern: 'admin', fromMe: false, desc: Lang.ADMINDESC}, (async (message, match) => {    
 
-    await message.sendMessage('💻Usage: *.ttp*\nℹ️Desc: Converts text to plain picture. \n\n💻Usage: *.attp*\nℹ️Desc:Adds rainbow effect to the text as a sticker. \n\n💻Usage: *.sttp*\nℹ️Desc: Makes a transparent sticker.\n\n💻Usage: *.animesay*\nℹ️Desc: It writes the text inside the banner the anime girl is holding.\n\n💻Usage: *.trumpsay*\nℹ️Desc: changes the text as if it is the tweet of Trump.\n\n💻Usage: *.changesay*\nℹ️Desc: Turns the text into the change my mind poster.\n\n💻Usage: *.plugin*\nℹ️Desc: Install external plugins.\n\n💻Usage: *.remove*\nℹ️Desc: Removes the plugin.\n\n💻Usage: *.invite*\nℹ️Desc: Provides invitation link of the group.\n\n💻Usage: *.locate*\nℹ️Desc: It will send the location of the bots device.\n\n💻Usage: *.afk*\nℹ️Desc: It makes the bot AFK - Away From Keyboard..\n\n💻Usage: *.term*\nℹ️Desc: Allows to run the command on the terminal of the server.\n\n💻Usage: *.restart*\nℹ️Desc: Restart the bot.\n\n💻Usage: *.shutdown*\nℹ️Desc: Shutdown the Bot.\n\n💻Usage: *.setvar*\nℹ️Desc: Set heroku config var.\n\n💻Usage: *.getvar*\nℹ️Desc: Get heroku config var.\n\n💻Usage: *.delvar*\nℹ️Desc: Delete heroku config var.\n\n💻Usage: *.filter*\nℹ️Desc: It adds a filter. If someone writes your filter, it send the answer. If you just write .filter, it will show your filter list.\n\n💻Usage: *.stop*\nℹ️Desc: Stops the filter you added previously.\n\n💻Usage: *.welcome*\nℹ️Desc: It sets the welcome message. If you leave it blank it shows the preset welcome message.\n\n💻Usage: *.goodbye*\nℹ️Desc: Sets the goodbye message. If you leave blank, it will show the preset goodbye message.\n\n💻Usage: *.addlydia*\nℹ️Desc: Activates Lydia (AI) for the tagged user.\n\n💻Usage: *.rmlydia*\nℹ️Desc: Makes Lydia disabled for the tagged user.\n\n💻Usage: *.kickme*\nℹ️Desc: It kicks the bot from the group where the command is given.\n\n💻Usage: *.pp*\nℹ️Desc: Makes the photo you reply the profile photo .\n\n💻Usage: *.block*\nℹ️Desc: Block tagged user..\n\n💻Usage: *.unblock*\nℹ️Desc: Unblock tagged user.\n\n💻Usage: *.jid*\nℹ️Desc: Gives the JID of the user.\n\n💻Usage *.tagall*\nℹ️Desc: Tags everyone in the group..\n\n💻Usage: *.update*\nℹ️Desc: Checks the update.\n\n💻Usage: *.update now*\nℹ️Desc: It updates the bot.\n\n💻Usage: *.deleteNotes*\nℹ️Desc: deletes all of your saved notes.\n\n💻Usage: *.save*\nℹ️Desc: To save a text as a note.\n\n💻Usage: *.spam*\nℹ️Desc: It will spam untill you stop it.\n\n💻Usage: *.killspam*\nℹ️Desc: Stops the spamming.');
+    await message.sendMessage('💻Usage: *.ttp*\nℹ️Desc: Converts text to plain picture. \n\n💻Usage: *.attp*\nℹ️Desc:Adds rainbow effect to the text as a sticker.\n\n💻Usage: *.animesay*\nℹ️Desc: It writes the text inside the banner the anime girl is holding.\n\n💻Usage: *.trumpsay*\nℹ️Desc: changes the text as if it is the tweet of Trump.\n\n💻Usage: *.changesay*\nℹ️Desc: Turns the text into the change my mind poster.\n\n💻Usage: *.plugin*\nℹ️Desc: Install external plugins.\n\n💻Usage: *.remove*\nℹ️Desc: Removes the plugin.\n\n💻Usage: *.invite*\nℹ️Desc: Provides invitation link of the group.\n\n💻Usage: *.locate*\nℹ️Desc: It will send the location of the bots device.\n\n💻Usage: *.afk*\nℹ️Desc: It makes the bot AFK - Away From Keyboard..\n\n💻Usage: *.term*\nℹ️Desc: Allows to run the command on the terminal of the server.\n\n💻Usage: *.restart*\nℹ️Desc: Restart the bot.\n\n💻Usage: *.shutdown*\nℹ️Desc: Shutdown the Bot.\n\n💻Usage: *.setvar*\nℹ️Desc: Set heroku config var.\n\n💻Usage: *.getvar*\nℹ️Desc: Get heroku config var.\n\n💻Usage: *.delvar*\nℹ️Desc: Delete heroku config var.\n\n💻Usage: *.filter*\nℹ️Desc: It adds a filter. If someone writes your filter, it send the answer. If you just write .filter, it will show your filter list.\n\n💻Usage: *.stop*\nℹ️Desc: Stops the filter you added previously.\n\n💻Usage: *.welcome*\nℹ️Desc: It sets the welcome message. If you leave it blank it shows the preset welcome message.\n\n💻Usage: *.goodbye*\nℹ️Desc: Sets the goodbye message. If you leave blank, it will show the preset goodbye message.\n\n💻Usage: *.addlydia*\nℹ️Desc: Activates Lydia (AI) for the tagged user.\n\n💻Usage: *.rmlydia*\nℹ️Desc: Makes Lydia disabled for the tagged user.\n\n💻Usage: *.kickme*\nℹ️Desc: It kicks the bot from the group where the command is given.\n\n💻Usage: *.pp*\nℹ️Desc: Makes the photo you reply the profile photo .\n\n💻Usage: *.block*\nℹ️Desc: Block tagged user..\n\n💻Usage: *.unblock*\nℹ️Desc: Unblock tagged user.\n\n💻Usage: *.jid*\nℹ️Desc: Gives the JID of the user.\n\n💻Usage *.tagall*\nℹ️Desc: Tags everyone in the group..\n\n💻Usage: *.update*\nℹ️Desc: Checks the update.\n\n💻Usage: *.update now*\nℹ️Desc: It updates the bot.\n\n💻Usage: *.deleteNotes*\nℹ️Desc: deletes all of your saved notes.\n\n💻Usage: *.save*\nℹ️Desc: To save a text as a note.\n\n💻Usage: *.spam*\nℹ️Desc: It will spam untill you stop it.\n\n💻Usage: *.killspam*\nℹ️Desc: Stops the spamming.');
 
 }));
 
@@ -82,7 +82,148 @@ Asena.addCommand({ pattern: 'trumpsay ?(.*)', fromMe: false, dontAddCommandList:
 
     }));
 
+Asena.addCommand({pattern: 'glitch ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.GLAT);
+  
+  var topText, bottomText;
+    if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        bottomText = split[1];
+        topText = split[0];
+}
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/tiktokeffect/?text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
+
+   await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg ,quoted: message.data})
+
+}));
+
+Asena.addCommand({pattern: 'neonglow ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/glowingneon/?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+   await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+
+Asena.addCommand({pattern: 'coffeecup ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/coffeecup/?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+
+Asena.addCommand({pattern: 'wood ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/woodblock/?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: '8bit ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.GLAT);
+  
+  var topText, bottomText;
+    if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        bottomText = split[1];
+        topText = split[0];
+}
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/8bit/?text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'shadow ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/shadowtext/?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'harrypotter ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/hpotter/?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'sparkling ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.GLAT);
+  
+  var topText, bottomText;
+    if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        bottomText = split[1];
+        topText = split[0];
+}
+
+    var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/sparkling?text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'watercolor ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/watercolor?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'ninjalogo ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/gaming?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'neonlight ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/neon_light?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: '3dtext ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/text3d?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
 
 
 
@@ -153,5 +294,151 @@ Asena.addCommand({ pattern: 'ptrumpsay ?(.*)', fromMe: true, dontAddCommandList:
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.png, caption: 'Made by WhatsAsena' })
 
     }));
+
+Asena.addCommand({pattern: 'pglitch ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.GLAT);
+  
+  var topText, bottomText;
+    if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        bottomText = split[1];
+        topText = split[0];
+}
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/tiktokeffect/?text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
+
+   await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg ,quoted: message.data})
+
+}));
+
+Asena.addCommand({pattern: 'pneonglow ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/glowingneon/?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+   await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+
+Asena.addCommand({pattern: 'pcoffeecup ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/coffeecup/?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+
+Asena.addCommand({pattern: 'pwood ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/woodblock/?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'p8bit ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.GLAT);
+  
+  var topText, bottomText;
+    if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        bottomText = split[1];
+        topText = split[0];
+}
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/8bit/?text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'pshadow ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/shadowtext/?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'pharrypotter ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://videfikri.com/api/textmaker/hpotter/?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'psparkling ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.GLAT);
+  
+  var topText, bottomText;
+    if (match[1].includes(';')) {
+        var split = match[1].split(';');
+        bottomText = split[1];
+        topText = split[0];
+}
+
+    var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/sparkling?text1=${topText}&text2=${bottomText}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'pwatercolor ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/watercolor?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'pninjalogo ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/gaming?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'pneonlight ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/neon_light?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+Asena.addCommand({pattern: 'p3dtext ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NGLAT);
+
+    var webimage = await axios.get(`https://docs-jojo.herokuapp.com/api/text3d?text=${match[1]}`, { responseType: 'arraybuffer' })
+
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg })
+
+}));
+
+
+
 
 
