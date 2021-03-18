f89343ae5f70268e3ea67e1137e13508facd2bfc
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-19 00:14:29 +0530

    Update instagram.js

--- plugins/instagram.js
+++ plugins/instagram.js
@@ -1,6 +1,6 @@
 /*
 # Copyright (C) 2020 MuhammedKpln.
-#
+#           edited and brought into action by Vai838
 # WhatsAsena is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
