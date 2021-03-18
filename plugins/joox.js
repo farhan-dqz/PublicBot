4e580fc5ffe8f549006ee100efa6f9f3b08cb7ba
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-19 00:15:00 +0530

    Update joox.js

--- plugins/joox.js
+++ plugins/joox.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2021 Vai838.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-WhatsAsena - Yusuf Usta
+WhatsAsenaDuplicated
 */
 
 const Asena = require('../events');
