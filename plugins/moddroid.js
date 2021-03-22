a4d70479f10a6471452bba9655955f1440bc458e
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-22 16:35:32 +0530

    Update moddroid.js

--- plugins/moddroid.js
+++ plugins/moddroid.js
@@ -11,7 +11,7 @@ const got = require('got');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
-Asena.addCommand({pattern: 'modd ?(.*)', fromMe: false, desc: Lang.MODD_DESC}, async (message, match) => {
+/*Asena.addCommand({pattern: 'modd ?(.*)', fromMe: false, desc: Lang.MODD_DESC}, async (message, match) => {
 	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
 	const url = `https://tobz-api.herokuapp.com/api/moddroid?q=${match[1]}&apikey=BotWeA`;
 	try {
@@ -23,6 +23,23 @@ Asena.addCommand({pattern: 'modd ?(.*)', fromMe: false, desc: Lang.MODD_DESC}, a
 		'*📝 ' + Lang.MODINFO +':* ```' + json.result[0].mod_info + '```\n\n' + 
 		'*📦 ' + Lang.SIZE +'* ```' + json.result[0].size + '```\n\n' + 
 		'*⬇️ ' + Lang.DOWNLOAD +':* ```' + json.result[0].download + '```\n', MessageType.text);
+	} catch {
+		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
+	}
+});*/
+
+
+Asena.addCommand({pattern: 'modd ?(.*)', fromMe: false, desc: Lang.MODD_DESC}, async (message, match) => {
+	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
+	const url = `https://docs-jojo.herokuapp.com/api/apk-pure?q=${match[1]}`;
+	try {
+		const response = await got(url);
+		const json = JSON.parse(response.body);
+		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, 
+		'*🏷️ ' + Lang.NAMEY +'* ```' + json.result[0].title + '```\n\n' +
+		'*📝 ' + Lang.MODINFO +':* ```' + json.result[0].version + '```\n\n' + 
+		'*📦 ' + Lang.SIZE +'* ```' + json.result[0].filesize + '```\n\n' + 
+		'*⬇️ ' + Lang.DOWNLOAD +':* ```' + json.result[0].url + '```\n', MessageType.text);
 	} catch {
 		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDMD, MessageType.text);
 	}
