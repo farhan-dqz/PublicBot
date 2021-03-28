f00f4b760e9e5d381c061a0f0fe9dc347a5bc8a4
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-03-28 16:57:56 +0530

    Update happymod.js

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -7,13 +7,14 @@ WhatsAsenaDuplicated
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
+const Config = require('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
 Asena.addCommand({pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 	if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
-	const url = `https://tobz-api.herokuapp.com/api/happymod?q=${match[1]}&apikey=kIKSEhfAfuMIonwfLGuY`;
+	const url = `https://tobz-api.herokuapp.com/api/happymod?q=${match[1]}&apikey=${Config.TOBZ_API_KEY}`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
