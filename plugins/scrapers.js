f3020391b3a75a50b76a39be70a4b3b013ac8d6a
farhan-dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-04-25 17:43:07 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -44,9 +44,7 @@ Asena.addCommand({pattern: 'trt(?: |$)(\\S*) ?(\\S*)', desc: Lang.TRANSLATE_DESC
 
     ceviri = await translatte(message.reply_message.message, {from: match[1] === '' ? 'auto' : match[1], to: match[2] === '' ? config.LANG : match[2]});
     if ('text' in ceviri) {
-        return await message.reply('*▶️ ' + Lang.LANG + ':* ```' + (match[1] === '' ? 'auto' : match[1]) + '```\n'
-        + '*◀️ ' + Lang.FROM + '*: ```' + (match[2] === '' ? config.LANG : match[2]) + '```\n'
-        + '*🔎 ' + Lang.RESULT + ':* ```' + ceviri.text + '```');
+        return await message.reply('*▶️ ' + Lang.RESULT + ':* ```' + ceviri.text + '```');
     } else {
         return await message.client.sendMessage(message.jid,Lang.TRANSLATE_ERROR,MessageType.text)
     }
