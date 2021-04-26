0da4e88546720dc6f8ad80bae4ef0edabac8b59b
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-04-26 22:23:48 +0530

    Update simsimih.js

--- plugins/simsimih.js
+++ plugins/simsimih.js
@@ -11,7 +11,7 @@ const got = require('got');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
-Asena.addCommand({pattern: 'bot ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
+Asena.addCommand({pattern: 'bot ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=false`;
 	try {
