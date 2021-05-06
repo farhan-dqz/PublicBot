8c90115b8883d99dba3a961effca9880e3b18bf5
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-05-06 10:02:00 +0530

    Update stickers.js

--- plugins/stickers.js
+++ plugins/stickers.js
@@ -79,34 +79,3 @@ Asena.addCommand({pattern: 'S', fromMe: true , dontAddCommandList: true}, (async
 }));
 
 
-
-Asena.addCommand({pattern: 'st', fromMe: true  , dontAddCommandList: true }, (async (message, match) => {    
-    if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);
-    var downloading = await message.client.sendMessage(message.jid,Lang.DOWNLOADING,MessageType.text);
-    var location = await message.client.downloadAndSaveMediaMessage({
-        key: {
-            remoteJid: message.reply_message.jid,
-            id: message.reply_message.id
-        },
-        message: message.reply_message.data.quotedMessage
-    });
-
-    if (message.reply_message.video === false && message.reply_message.image) {
-        execFile(cwebp, [location, '-o', 'output.webp'], async err => {
-            if (err) {
-                throw err;
-            }
-        
-            await message.sendMessage(fs.readFileSync('./output.webp'), MessageType.sticker);
-        });
-        return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
-    }
-
-    ffmpeg(location)
-        .outputOptions(["-y", "-vcodec libwebp", "-lossless 1", "-qscale 1", "-preset default", "-loop 0", "-an", "-vsync 0", "-s 512x512"])
-        .save('sticker.webp')
-        .on('end', async () => {
-            await message.sendMessage(fs.readFileSync('sticker.webp'), MessageType.sticker);
-        });
-    return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
-}));
