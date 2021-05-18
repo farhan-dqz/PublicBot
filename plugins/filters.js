c532518df479727fbf3be08d0ee5fefff66c5cf8
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-05-18 23:23:25 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -5,7 +5,7 @@ you may not use this file except in compliance with the License.
 
 WhatsAsena - Yusuf Usta
 */
-
+const fs = require('fs')
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const FilterDb = require('./sql/filters');
