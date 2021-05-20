71ed91cd26df660ec08e1645673766b0b6ae6a4b
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-05-20 11:26:08 +0530

    Update nekoanime.js

--- plugins/nekoanime.js
+++ plugins/nekoanime.js
@@ -4,7 +4,7 @@ const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
 
-    Asena.addCommand({ pattern: 'nekoanime', fromMe: true }, async (message, match) => {
+    Asena.addCommand({ pattern: 'nekoanime', fromMe: false }, async (message, match) => {
 
         if (message.jid === '905524317852-1612300121@g.us') {
 
