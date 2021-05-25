69d6b5512a926af3b26a699b0a687a954448d262
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-05-25 13:20:46 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -1,40 +1,20 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-Developer & Co-Founder - Phaticusthiccy
-*/
-
-const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const {spawnSync} = require('child_process');
-const Config = require('../config');
-const chalk = require('chalk');
-const Axios = require('axios');
-
-const Language = require('../language');
-const Lang = Language.getString('system_stats');
-
-
-    Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
         if (Config.ALIVEMSG == 'default') {
-            await message.client.sendMessage(message.jid,'```I AM  Alive!```\n\n ```Type``` *.help* ```for command list``` \n\n ```Coded By``` *✭𝕱𝖆𝖗𝖍𝖆𝖓║𝕯𝖖𝖟 \n\n💙*, MessageType.text);
+            await message.client.sendMessage(message.jid,'```I Am Alive```\n\n ```Type``` *.help* ```for command list``` \n\n ```Coded By``` *✭𝕱𝖆𝖗𝖍𝖆𝖓║𝕯𝖖𝖟* \n\n: MessageType.text);
         }
         else {
-            const pow = '*Public Bot*'
+            const pow = '*PublicBot🧞‍♂️*'
             const payload = Config.ALIVEMSG
             const status = await message.client.getStatus()
             const ppUrl = await message.client.getProfilePicture() 
             const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
 
             if (!payload.includes('{pp}')) {
-                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow, MessageType.text);
+                await message.client.sendMessage(message.jid,payload.replace('{ + '\n' + pow, MessageType.text);
             }
             else if (payload.includes('{pp}')) {
-                await message.sendMessage(Buffer(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow });
+                await message.sendMessage(Buffer(resim.data), MessageType.image, { caption: payload.replace('{+ '\n' + pow });
             }
         }
     }));
