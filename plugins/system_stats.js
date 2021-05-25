2346d633134df5c475b19700c26942110e155b86
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-05-25 12:26:12 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -1,50 +1,24 @@
-/* Copyright (C) 2020 Yusuf Usta.
+Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
+        if (Config.ALIVEMSG == 'default') {
+            await message.client.sendMessage(message.jid,'```I AM  Alive!```\n\n ```Type``` *.help* ```for command list``` \n\n ```Coded By``` *✭𝕱𝖆𝖗𝖍𝖆𝖓║𝕯𝖖𝖟 🧞‍♂️*\n\n', MessageType.text);
+        }
+        else {
+            const payload = Config.ALIVEMSG
+            const status = await message.client.getStatus()
+            const ppUrl = await message.client.getProfilePicture() 
+            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
 
-WhatsAsena - Yusuf Usta
-Developer & Co-Founder - Phaticusthiccy
-*/
+            if (!payload.includes('{pp}')) {
+                await message.client.sendMessage(message.jid,payload.replace',+ '\n' });
+            }
+        }
+    }));
 
-const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const {spawnSync} = require('child_process');
-const Config = require('../config');
-const chalk = require('chalk');
+    Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
 
-const Language = require('../language');
-const Lang = Language.getString('system_stats');
-
-Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
-    if (Config.ALIVEMSG == 'default') {
-        await message.client.sendMessage(message.jid,'```I AM  Alive!```\n\n ```Type``` *.help* ```for command list``` \n\n ```Coded By``` *✭𝕱𝖆𝖗𝖍𝖆𝖓║𝕯𝖖𝖟 \n\n💙*' , MessageType.text);
-    }
-    else {
-        await message.client.sendMessage(message.jid,Config.ALIVEMSG, MessageType.text);
-    }
-}));
-
-Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
-    const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-    await message.sendMessage(
-        '```' + child + '```', MessageType.text
-    );
-}));
-
-
-Asena.addCommand({pattern: 'palive', fromMe: true, dontAddCommandList: true }, (async (message, match) => {
-    if (Config.ALIVEMSG == 'default') {
-        await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/whatsasenaremaster' , MessageType.text);
-    }
-    else {
-        await message.client.sendMessage(message.jid,Config.ALIVEMSG, MessageType.text);
-    }
-}));
-
-Asena.addCommand({pattern: 'psysd', fromMe: true , dontAddCommandList: true}, (async (message, match) => {
-    const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-    await message.sendMessage(
-        '```' + child + '```', MessageType.text
-    );
-}));
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
