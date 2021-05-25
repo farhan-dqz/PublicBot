6f044ed45645ac0c8e3264b068a3a28ea8ce7b1c
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-05-25 12:58:15 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -1,4 +1,24 @@
-        Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+/* Copyright (C) 2020 Yusuf Usta.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+Developer & Co-Founder - Phaticusthiccy
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const {spawnSync} = require('child_process');
+const Config = require('../config');
+const chalk = require('chalk');
+const Axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('system_stats');
+
+
+    Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
         if (Config.ALIVEMSG == 'default') {
             await message.client.sendMessage(message.jid,'```I AM  Alive!```\n\n ```Type``` *.help* ```for command list``` \n\n ```Coded By``` *✭𝕱𝖆𝖗𝖍𝖆𝖓║𝕯𝖖𝖟 \n\n💙*, MessageType.text);
