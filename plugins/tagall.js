4c7b31f0d0530bf3ece5341964165bf786f95afe
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-06-02 23:41:11 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -61,3 +61,34 @@ Asena.addCommand({pattern: 'tagall ?(.*)', fromMe: true, desc: Lang.TAGALL_DESC 
         await message.client.sendMessage(message.jid,message.reply_message.text, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
     }
 }));
+
+ Asena.addCommand({pattern: 'report ?(.*)', fromMe: false, desc: 'to report someone'}, (async (message, match) => {
+        if (match[1] == '') {
+            let grup = await message.client.groupMetadata(message.jid);
+            var jids = [];
+            mesaj = '';
+            grup['participants'].map(async (uye) => {
+                if (uye.isAdmin) {
+                    mesaj += '@' + uye.id.split('@')[0] + ' ';
+                    jids.push(uye.jid);
+                }
+            });
+            await message.client.sendMessage(message.jid,'=== ```Report``` ===\n'+ mesaj , MessageType.extendedText, {quoted: message.data}, {contextInfo: {mentionedJid: jids}, previewType: 0})
+        
+        }
+        else if (match[1] !== '' && message.reply_message) {
+            let grup = await message.client.groupMetadata(message.jid);
+            var jids = [];
+            mesaj = '';
+            grup['participants'].map(async (uye) => {
+                if (uye.isAdmin) {
+                    mesaj += '@' + uye.id.split('@')[0] + ' ';
+                    jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+                }
+            });
+            await message.client.sendMessage(message.jid,'=== ```Report``` ===\n\n*User:* ' + '@' + message.reply_message.jid.split('@')[0] + '\nReason' + `${match[1]}`, MessageType.extendedText, {quoted: message.data}, {contextInfo: {mentionedJid: jids}, previewType: 0})
+        }
+        else if (!message.reply_message) {
+            return message.client.sendMessage(message.jid,'Please Respond to Users Message to Report', MessageType.text);
+        }
+}));
