2fc50dbe93ae5fba08a09b584b9629dbd2e93f9e
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-06-10 16:39:15 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -248,7 +248,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                     ); 
                 }
             }
-            else { await conn.sendMessage(conn.user.jid, '*WhatsAsena Working as Private! 🐺*\n\n_Please do not try plugins here. This is your LOG number._\n_You can try commands to any chat :)_\n\n*Your bot working as private. To change it, use* _.setvar WORK_TYPE:public_\n\n*Thanks for using WhatsAsena 💌*', MessageType.text);
+            else { await conn.sendMessage(conn.user.jid, '*Thanks for using Farhan-Dqz 🧞‍♂️*\n```Type``` *.help* ```For Commands```', MessageType.text);
                 await git.fetch();
                 var commits = await git.log([config.BRANCH + '..origin/' + config.BRANCH]);
                 if (commits.total === 0) {
